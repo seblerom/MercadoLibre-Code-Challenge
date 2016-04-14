ebbec856874cd5d70fd118f221db3aec51a02ff0
Sebastian Leon <soloban88@gmail.com>
2016-04-14 17:45:38 -0300

    Adding model - I gotta run  to the university

--- MercadoPago/MercadoPago/MetodoDePago.swift
+++ MercadoPago/MercadoPago/MetodoDePago.swift
@@ -16,6 +16,48 @@ class MetodoDePago: UIViewController {
         let parameters = ["public_key":public_key]
         Connection.networkingCallsMaster(paymentMethods, parameters: parameters) { response in
             print(response)
+            self.CreateArrayWithPaymentMethods(response)
+        }
+    }
+    
+    func CreateArrayWithPaymentMethods(data:(JSON)){
+        
+        for (index,subJson):(String, JSON) in data {
+            let id = subJson["id"].stringValue
+            let deferred_capture = subJson["deferred_capture"].stringValue
+            
+            
+            var additionalInfoNeeded = [String]()
+            if !subJson["additional_info_needed"].isEmpty {
+                for item in subJson["additional_info_needed"] {
+                    additionalInfoNeeded.append(String(item))
+                }
+            }
+            
+        
+            
+            
+            
+            
+            
         }
     }
 }
+
+
+/*
+ 
+ var id:String?
+ var deferred_capture:String?
+ var additional_info_needed = [String]()
+ var payment_type_id:String?
+ var accreditation_time:Int?
+ var settings = [String]()
+ 
+ var max_allowed_amount:Int?
+ var min_allowed_amount:Int?
+ var secure_thumbnail:String?
+ var thumbnail:String?
+ var name:String?
+ var status:String?
+ */
--- MercadoPago/MercadoPago/PaymentMethod.swift
+++ MercadoPago/MercadoPago/PaymentMethod.swift
@@ -11,6 +11,67 @@ import UIKit
 class PaymentMethod: NSObject {
     
     
+    var id:String?
+    var deferred_capture:String?
+    var additional_info_needed = [String]()
+    var payment_type_id:String?
+    var accreditation_time:Int?
+    var settings = [String]()
     
-
+    var max_allowed_amount:Int?
+    var min_allowed_amount:Int?
+    var secure_thumbnail:String?
+    var thumbnail:String?
+    var name:String?
+    var status:String?
+    
+    struct bin {
+        var pattern:String?
+        var installments_pattern:String?
+        var exclusion_pattern:String?
+        
+        init(pattern:String,installments_pattern:String,exclusion_pattern:String){
+            self.pattern = pattern
+            self.installments_pattern = installments_pattern
+            self.exclusion_pattern = exclusion_pattern
+        }
+    }
+    
+    struct card_number {
+        var validation:String?
+        var length:Int?
+        
+        init(validation:String,length:Int){
+            self.validation = validation
+            self.length = length
+        }
+    }
+    
+    struct security_code {
+        var mode:String?
+        var card_location:String?
+        var length:Int?
+        
+        init(mode:String,card_location:String,length:Int){
+            self.mode = mode
+            self.card_location = card_location
+            self.length = length
+        }
+    }
+    
+    init(id:String,deferred_capture:String,additional_info_needed:([String]),payment_type_id:String,accreditation_time:Int,settings:([String]),max_allowed_amount:Int,min_allowed_amount:Int,secure_thumbnail:String,thumbnail:String,name:String,status:String){
+    
+        self.id = id
+        self.deferred_capture = deferred_capture
+        self.additional_info_needed = additional_info_needed
+        self.payment_type_id = payment_type_id
+        self.accreditation_time = accreditation_time
+        self.settings = settings
+        self.max_allowed_amount = max_allowed_amount
+        self.min_allowed_amount = min_allowed_amount
+        self.secure_thumbnail = secure_thumbnail
+        self.thumbnail = thumbnail
+        self.name = name
+        self.status = status
+    }
 }
