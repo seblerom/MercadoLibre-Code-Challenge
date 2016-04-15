4d48ddd4eddd074b7cfbf8d4e5a016f6faa66943
Sebastian Leon <soloban88@gmail.com>
2016-04-15 17:18:56 -0300

    Adding third view controller Cuotas

---
+++ MercadoPago/MercadoPago/Cuotas.swift
@@ -0,0 +1,13 @@
+//
+//  Cuotas.swift
+//  MercadoPago
+//
+//  Created by alvaro sebastian leon romero on 4/15/16.
+//  Copyright © 2016 seblerom. All rights reserved.
+//
+
+import UIKit
+
+class Cuotas: UITableViewController {
+
+}
---
+++ MercadoPago/MercadoPago/CuotasCell.swift
@@ -0,0 +1,13 @@
+//
+//  CuotasCell.swift
+//  MercadoPago
+//
+//  Created by alvaro sebastian leon romero on 4/15/16.
+//  Copyright © 2016 seblerom. All rights reserved.
+//
+
+import UIKit
+
+class CuotasCell: UITableViewCell {
+
+}
